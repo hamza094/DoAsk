72fa3c903edda59e54d3e32e4d489feff0f551e2
hamza <hamza_pisces@live.com>
2019-05-07 18:48:50 +0500

    files

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -21,6 +21,16 @@
                   </form>
               </div>
               <button class="btn btn-success btn-sm">Login</button>
+              <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
           </div>
           <hr>
            @if(count($trending ))
